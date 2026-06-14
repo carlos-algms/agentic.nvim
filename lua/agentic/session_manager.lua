04b2dce72520d28017dd53ec613e68d82c858afc
Carlos Gomes <carlos.algms@gmail.com>
2026-06-14 17:01:36 +0200

    extract environment_info

--- lua/agentic/session_manager.lua
+++ lua/agentic/session_manager.lua
@@ -12,6 +12,7 @@ local DiagnosticsList = require("agentic.ui.diagnostics_list")
 local FileSystem = require("agentic.utils.file_system")
 local Logger = require("agentic.utils.logger")
 local SlashCommands = require("agentic.acp.slash_commands")
+local EnvironmentInfo = require("agentic.utils.environment_info")
 
 --- @class agentic._SessionManagerPrivate
 local P = {}
@@ -762,7 +763,7 @@ function SessionManager:_handle_input_submit(input_text)
 
         table.insert(prompt, {
             type = "text",
-            text = self:_get_system_info(),
+            text = EnvironmentInfo.get_system_info(),
         })
     end
 
@@ -1309,70 +1310,6 @@ function SessionManager:_handle_new_config_options(new_config_options)
     if self.config_options.mode and self.config_options.mode.currentValue then
         self:_set_mode_to_chat_header(self.config_options.mode.currentValue)
     end
-end
-
-function SessionManager:_get_system_info()
-    local os_name = vim.uv.os_uname().sysname
-    local os_version = vim.uv.os_uname().release
-    local os_machine = vim.uv.os_uname().machine
-    local shell = os.getenv("SHELL")
-    local neovim_version = tostring(vim.version())
-    local today = os.date("%Y-%m-%d")
-
-    local res = string.format(
-        [[
-- Platform: %s-%s-%s
-- Shell: %s
-- Editor: Neovim %s
-- Current date: %s]],
-        os_name,
-        os_version,
-        os_machine,
-        shell,
-        neovim_version,
-        today
-    )
-
-    local project_root = vim.uv.cwd()
-
-    local git_root = vim.fs.root(project_root or 0, ".git")
-    if git_root then
-        project_root = git_root
-        res = res .. "\n- This is a Git repository."
-
-        local branch =
-            vim.fn.system("git rev-parse --abbrev-ref HEAD"):gsub("\n", "")
-        if vim.v.shell_error == 0 and branch ~= "" then
-            res = res .. string.format("\n- Current branch: %s", branch)
-        end
-
-        local changed = vim.fn.system("git status --porcelain"):gsub("\n$", "")
-        if vim.v.shell_error == 0 and changed ~= "" then
-            local files = vim.split(changed, "\n")
-            res = res .. "\n- Changed files:"
-            for _, file in ipairs(files) do
-                res = res .. "\n  - " .. file
-            end
-        end
-
-        local commits = vim.fn
-            .system("git log -3 --oneline --format='%h (%ar) %an: %s'")
-            :gsub("\n$", "")
-        if vim.v.shell_error == 0 and commits ~= "" then
-            local commit_lines = vim.split(commits, "\n")
-            res = res .. "\n- Recent commits:"
-            for _, commit in ipairs(commit_lines) do
-                res = res .. "\n  - " .. commit
-            end
-        end
-    end
-
-    if project_root then
-        res = res .. string.format("\n- Project root: %s", project_root)
-    end
-
-    res = "<environment_info>\n" .. res .. "\n</environment_info>"
-    return res
 end
 
 function SessionManager:destroy()
